7376d1b75db502b9643b057eb6b4db256029e0b7
carlos rodriguez <88858893+twincarlos@users.noreply.github.com>
2022-06-01 13:54:11 -0600

    Update category_details.py

--- app/seeds/category_details.py
+++ app/seeds/category_details.py
@@ -11,6 +11,11 @@ def seed_category_details():
     chick_fil_a_fast_food = Category_Detail(category_id=2, restaurant_id=6)
     cold_stone_ice_cream = Category_Detail(category_id=3, restaurant_id=7)
     blaze_pizza = Category_Detail(category_id=4, restaurant_id=8)
+    
+    juan_valdez_coffee = Category_Detail(category_id=1, restaurant_id=9)
+    wendys_fast_food = Category_Detail(category_id=2, restaurant_id=10)
+    nitrogen_ice_cream = Category_Detail(category_id=3, restaurant_id=11)
+    mod_pizza = Category_Detail(category_id=4, restaurant_id=12)
 
     db.session.add(starbucks_coffee)
     db.session.add(mcdonalds_fast_food)
@@ -21,6 +26,11 @@ def seed_category_details():
     db.session.add(chick_fil_a_fast_food)
     db.session.add(cold_stone_ice_cream)
     db.session.add(blaze_pizza)
+    
+    db.session.add(juan_valdez_coffee)
+    db.session.add(wendys_fast_food)
+    db.session.add(nitrogen_ice_cream)
+    db.session.add(mod_pizza)
 
     db.session.commit()
 
